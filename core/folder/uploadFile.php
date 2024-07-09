bc46f52b220be86bccd209e016ec14182f5b1e41
VietLinh1104 <linhtx2004@gmail.com>
2024-07-10 02:48:31 +0700

    cmt

--- core/folder/uploadFile.php
+++ core/folder/uploadFile.php
@@ -1,18 +1,27 @@
 <?php
+
+    // truyền file upload vào hàm uploadFile và gán id
     function uploadFile($fileType, $id){
 
-
+        // kiểm tra xem nhận đc file chưa và có lỗi không
         if (isset($_FILES[$fileType]) && $_FILES[$fileType]["error"] == 0) {
+
+            // lưu file vào biến filename
             $filename = $_FILES[$fileType]["name"];
+
+            // lấy định dạng file (png , gif , jpeg)
             $fileExtension = pathinfo($filename, PATHINFO_EXTENSION);
     
+            // tạo ra tên mới cho file => imgPath.png
             $newFilename =  $fileType.".". $fileExtension;
     
-            
+            // tạo đường link upload/id/
             $idPath = createDirectory("upload/".$id."/");
     
+            // lưu file ảnh vào đường dẫn => upload/id/imgPath.png
             $success = move_uploaded_file($_FILES[$fileType]["tmp_name"], "upload/".$id."/". $newFilename);
 
+            // vidu xuat ra string => "7175376b090c1d172c9acce5116533f3/imgPath.png"
             $filePath = $id."/". $newFilename;
 
             if ($success) {
